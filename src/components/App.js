9f756330cc2617ec8bd99e7d722cbfec794c945f
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-06 21:20:08 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import { useState } from "react";
 
 function App() {
+  const [colorMode, setColorMode] = useState(false);
+
+  const [selectedCategory, setSelectCategory] = useState("All");
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = colorMode ? "App dark" : "App light"
+
+
+  function handleColorMode () {
+    setColorMode(ColorMode => !ColorMode)
+  }
+
+  function handleCategory (event) {
+   setSelectCategory(event.target.value);
+   console.log(selectedCategory)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleColorMode}>{colorMode ? "Dark Mode": "Light Mode"}</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={itemData} selectedCategory = {selectedCategory}  onChangeCategory={handleCategory}/>
     </div>
   );
 }
